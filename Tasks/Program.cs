b83db18428eb858b874027a03bf443fac96ae6d9
«Juliya <yuliya.jeber@yandex.ru>
2022-07-07 14:17:19 +0300

    add Tasks folder and gitignore file

---
+++ Tasks/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 2:
+
+// Console.WriteLine("Введите первое число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+// bool maxA = numberA > numberB;
+// bool maxB = numberA < numberB;
+// bool maxC = numberA == numberB;
+
+// if (maxA)
+// {
+// Console.WriteLine($"Число {numberA} больше, чем {numberB}"); 
+// }
+// if (maxC)
+// {
+//  Console.WriteLine($"Числа {numberA} и {numberB} равны");
+// }
+// else
+// {
+//  Console.WriteLine($"Число {numberB} больше, чем {numberA}");
+// }
+
+
+// Задача 4:
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Максимальное число равно: ");
+// Console.WriteLine(Math.Max(a, Math.Max(b, c)));
